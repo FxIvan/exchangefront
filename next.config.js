131f24586a9abd7b34a44d64ce7bf8e58cc47546
Ivan Ariel Almendra <almendraivan210814@gmail.com>
2023-01-20 17:09:18 -0300

    instalando taiwilind

--- next.config.js
+++ next.config.js
@@ -1,41 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withVideos = require('next-videos')
-
-/** @type {import('tailwindcss').Config} */
-
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
-module.exports = withVideos({
-  basePath: '/v2',
-
-  webpack(config, { isServer }) {
-    const prefix = config.assetPrefix ?? config.basePath ?? '';
-    config.module.rules.push({
-      test: /\.mp4$/,
-      use: [{
-        loader: 'file-loader',
-        options: {
-          publicPath: `${prefix}/_next/static/media/`,
-          outputPath: `${isServer ? '../' : ''}static/media/`,
-          name: '[name].[hash].[ext]',
-        },
-      }],
-    });
-
-    return config;
-  }
-})
 
 const nextConfig = {
   reactStrictMode: true,
