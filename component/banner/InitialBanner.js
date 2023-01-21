020afc9412a857ebe7568eaef3597c5c21d66bf5
Ivan Ariel Almendra <almendraivan210814@gmail.com>
2023-01-20 23:42:04 -0300

    fondo de banner con titulo h1

--- component/banner/InitialBanner.js
+++ component/banner/InitialBanner.js
@@ -1,18 +1,41 @@
+import Image from "next/image";
 import { Container, Row } from "react-bootstrap";
 import styles from "./css/Banner.module.css";
+import hedragon from "../../public/images/hedronimg.svg";
 
 export default function InitialBanner() {
   return (
-    <Container fluid className={styles.containerBanner}>
-      <Container>
+    <Container
+      fluid
+      className={styles.containerBanner + " bg-black text-right"}
+    >
+      <Image
+        src={hedragon.src}
+        width={100}
+        height={100}
+        className={styles.imghedron}
+      />
+
+      <Image
+        src={hedragon.src}
+        width={100}
+        height={100}
+        className={styles.imghedron2}
+      />
+
+      <Container className={styles.subcontainer}>
         <Row>
-          <Row>
-            <h2>WELCOME TO CRONO</h2>
+          <Row className="pb-5">
+            <h2 className={styles.h2crono}>WELCOME TO CRONO</h2>
           </Row>
-          <Row><h1>The Internet of Blockchains.</h1></Row>
-          <Row>
+          <Row className={"pb-5 " + styles.h1Style}>
+            <h1>
+              The Internet of <br /> Blockchains.
+            </h1>
+          </Row>
+          <Row className="pb-5">
             <p>
-              Cronos is an ever expanding ecosystem of connected apps and
+              Cronos is an ever expanding ecosystem of connected <br /> apps and
               services, built for a decentralized future.
             </p>
           </Row>
